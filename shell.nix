d462b8c57a3f0434854d8585c71e585689df0a4a
Tate Johnson <tate@tatey.com>
2024-01-04 21:46:06 +1000

    Update nix.
    - Ruby 3.2.
    - Bundler 2.

--- shell.nix
+++ shell.nix
@@ -1,13 +1,14 @@
 let
   nixpkgs =
     import (builtins.fetchTarball {
-      url = https://github.com/NixOS/nixpkgs/archive/2362848adf8def2866fabbffc50462e929d7fffb.tar.gz;
+      url = https://github.com/NixOS/nixpkgs/archive/d44d59d2b5bd694cd9d996fd8c51d03e3e9ba7f7.tar.gz;
     }) { };
 in
 nixpkgs.mkShell {
   buildInputs = [
+    nixpkgs.ruby_3_2
+    nixpkgs.bundler
     nixpkgs.awscli2
-    nixpkgs.ruby_3_1
   ];
   shellHook = ''
     eval "$(direnv hook $SHELL)"
