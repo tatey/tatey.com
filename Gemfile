b63408803d3c76daf4925743f668bd1a17987226
Tate Johnson <tate@tatey.com>
2025-01-06 23:29:53 +1000

    Update ruby to 3.3 and nix to latest unstable.

--- Gemfile
+++ Gemfile
@@ -1,4 +1,6 @@
 source 'https://rubygems.org'
 
+gem "base64"
+gem "csv"
 gem 'jekyll'
 gem 'webrick'
--- shell.nix
+++ shell.nix
@@ -1,14 +1,15 @@
 let
   nixpkgs =
     import (builtins.fetchTarball {
-      url = https://github.com/NixOS/nixpkgs/archive/d44d59d2b5bd694cd9d996fd8c51d03e3e9ba7f7.tar.gz;
+      url = https://github.com/NixOS/nixpkgs/archive/bf689c40d035239a489de5997a4da5352434632e.tar.gz;
     }) { };
 in
 nixpkgs.mkShell {
   buildInputs = [
-    nixpkgs.ruby_3_2
+    nixpkgs.ruby_3_3
     nixpkgs.bundler
     nixpkgs.awscli2
+    nixpkgs.libffi
   ];
   shellHook = ''
     eval "$(direnv hook $SHELL)"
